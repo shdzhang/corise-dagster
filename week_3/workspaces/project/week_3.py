e513d62390b4ab9683f207cdb6892e5b4f8141cd
shdzhang <39942190+shdzhang@users.noreply.github.com>
2022-12-11 13:57:45 +0800

    Update week_3.py

--- week_3/workspaces/project/week_3.py
+++ week_3/workspaces/project/week_3.py
@@ -4,6 +4,7 @@ from dagster import (
     In,
     Nothing,
     Out,
+    String,
     ResourceDefinition,
     RetryPolicy,
     RunRequest,
@@ -20,34 +21,41 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@op(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"}
+)
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    return [Stock.from_list(stock) for stock in context.resources.s3.get_data(s3_key)]
 
 
 @op
-def process_data():
-    # You can reuse the logic from the previous week
-    pass
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    # get the stock with the highest number of stock.high
+    highest_stock = max(stocks, key=lambda stock:stock.high)
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
 
 
-@op
-def put_redis_data():
-    # You can reuse the logic from the previous week
-    pass
+@op (
+    required_resource_keys={"redis"}
+)
+def put_redis_data(context, stock_aggregation: Aggregation) -> Nothing:
+    context.resources.redis.put_data(name=str(stock_aggregation.date), value=str(stock_aggregation.high))
 
 
-@op
-def put_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@op (
+    required_resource_keys={"s3"}
+)
+def put_s3_data(context, stock_aggregation: Aggregation) -> Nothing:
+    context.resources.s3.put_data(key=stock_aggregation.date, data=stock_aggregation)
 
 
 @graph
 def week_3_pipeline():
-    # You can reuse the logic from the previous week
-    pass
+    high_stocks = process_data(get_s3_data())
+    put_redis_data(high_stocks)
+    put_s3_data(high_stocks)
 
 
 local = {
@@ -75,28 +83,67 @@ docker = {
     "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock_9.csv"}}},
 }
 
-
-def docker_config():
-    pass
+@static_partitioned_config(
+    partition_keys=[str(i) for i in range(1, 11)]
+)
+def docker_config(partition_key: String):
+    docker_config = docker.copy()
+    docker_config['ops'] = {"get_s3_data": {"config": {"s3_key": f"prefix/stock_{partition_key}.csv"}}}
+    return docker_config
 
 
 week_3_pipeline_local = week_3_pipeline.to_job(
-    name="week_3_pipeline_local",
+    name = "week_3_pipeline_local",
+    config = local,
+    resource_defs = {
+        "s3": mock_s3_resource,
+        "redis": ResourceDefinition.mock_resource()
+    }
 )
 
 week_3_pipeline_docker = week_3_pipeline.to_job(
-    name="week_3_pipeline_docker",
+    name = "week_3_pipeline_docker",
+    resource_defs = {
+        "s3": s3_resource,
+        "redis": redis_resource
+    },
+    op_retry_policy = RetryPolicy(max_retries=10, delay=1)
 )
 
 
-week_3_schedule_local = None
+week_3_schedule_local = ScheduleDefinition(
+    job = week_3_pipeline_local, 
+    cron_schedule = "*/15 * * * *"
+)
 
 
-@schedule
+@schedule(
+    job=week_3_pipeline_docker,
+    cron_schedule="0 * * * *"
+)
 def week_3_schedule_docker():
-    pass
+    return RunRequest(
+        run_config=docker_config       
+    )
 
 
-@sensor
-def week_3_sensor_docker():
-    pass
+@sensor(
+    job=week_3_pipeline_docker,
+    minimum_interval_seconds=30
+)
+def week_3_sensor_docker(context):
+    s3_keys = get_s3_keys(
+        bucket="dagster",
+        prefix="prefix",
+        endpoint_url="http://localstack:4566"
+    )
+    if not s3_keys:
+        yield SkipReason("No new s3 files found in bucket.")
+    else:
+        for s3_key in s3_keys:
+            docker_config = docker.copy()
+            docker_config['ops'] = {"get_s3_data": {"config": {"s3_key": f"{s3_key}"}}}
+            yield RunRequest(
+                run_key=s3_key, 
+                run_config=docker_config
+            )

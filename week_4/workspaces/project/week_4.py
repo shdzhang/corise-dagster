49ec6f807763a96256aafd6dc86e2a912ce3b4f7
shdzhang <39942190+shdzhang@users.noreply.github.com>
2022-12-18 11:57:45 +0800

    Update for week4 project

--- week_4/workspaces/project/week_4.py
+++ week_4/workspaces/project/week_4.py
@@ -5,28 +5,61 @@ from workspaces.resources import redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@asset
-def get_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    group_name="corise"
+)
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    return [Stock.from_list(stock) for stock in context.resources.s3.get_data(s3_key)]
 
 
-@asset
-def process_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    group_name="corise"
+)
+def process_data(stocks: List[Stock]) -> Aggregation:
+    # get the stock with the highest number of stock.high
+    highest_stock = max(stocks, key=lambda stock:stock.high)
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
 
 
-@asset
-def put_redis_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    required_resource_keys={"redis"},
+    group_name="corise"
+)
+def put_redis_data(context, stock_aggregation: Aggregation) -> Nothing:
+    context.resources.redis.put_data(name=str(stock_aggregation.date), value=str(stock_aggregation.high))
 
 
-@asset
-def put_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@asset(
+    required_resource_keys={"s3"},
+    group_name="corise"
+)
+def put_s3_data(context, stock_aggregation: Aggregation) -> Nothing:
+    context.resources.s3.put_data(key=stock_aggregation.date, data=stock_aggregation)
 
 
-get_s3_data_docker, process_data_docker, put_redis_data_docker, put_s3_data_docker = with_resources()
+get_s3_data_docker, process_data_docker, put_redis_data_docker, put_s3_data_docker = with_resources(
+    definitions=[get_s3_data, process_data, put_redis_data, put_s3_data],
+    resource_defs={
+        "s3": s3_resource, 
+        "redis": redis_resource
+    },
+    resource_config_by_key={
+        "s3": {
+            "config": {
+                "bucket": "dagster",
+                "access_key": "test",
+                "secret_key": "test",
+                "endpoint_url": "http://localstack:4566",
+            }
+        },
+        "redis": {
+            "config": {
+                "host": "redis",
+                "port": 6379,
+            }
+        }
+    }
+)
